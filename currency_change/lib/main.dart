e4150b6c343253ebead4fe3ced7cce7e63b45915
André Siqueira <siqueira.andre@gmail.com>
2019-08-15 01:55:13 -0300

    keyboard decimal in IOS

--- currency_change/lib/main.dart
+++ currency_change/lib/main.dart
@@ -143,7 +143,7 @@ Widget buildTextField(String label, String prefix, TextEditingController c, Func
         prefixText: prefix),
     style: TextStyle(color: Colors.amber, fontSize: 25.0),
     onChanged: f,
-    keyboardType: TextInputType.number,
+    keyboardType: TextInputType.numberWithOptions(decimal: true),
   );
 }
 
