1f45719acd7e14f6b62584de16dadd4c17ffb910
André Siqueira <siqueira.andre@gmail.com>
2019-08-15 01:50:35 -0300

    resetar campos se for vazio

--- currency_change/lib/main.dart
+++ currency_change/lib/main.dart
@@ -34,21 +34,45 @@ class _HomeState extends State<Home> {
   double euro;
 
   void _realChanged(String text){
+
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+
     double real = double.parse(text);
     dolarController.text = (real/dolar).toStringAsFixed(2);
     euroController.text = (real/euro).toStringAsFixed(2);
   }
 
   void _dolarChanged(String text){
+
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+
     double dolar = double.parse(text);
     realController.text = (dolar * this.dolar).toStringAsFixed(2);
     euroController.text = (dolar * this.dolar / euro).toStringAsFixed(2);
   }
 
   void _euroChanged(String text){
+
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+
     double euro = double.parse(text);
     realController.text = (euro * this.euro).toStringAsFixed(2);
     dolarController.text = (euro * this.euro / dolar).toStringAsFixed(2);
+  }
+
+  void _clearAll(){
+    realController.text = "";
+    dolarController.text = "";
+    euroController.text = "";
   }
 
   @override
