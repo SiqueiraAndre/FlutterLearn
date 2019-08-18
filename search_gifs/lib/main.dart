d6f9074b4e3f5fd13871ed4027dffc81f241f7d1
André Siqueira <siqueira.andre@gmail.com>
2019-08-18 09:58:01 -0300

    start seach gifs

---
+++ search_gifs/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'package:search_gifs/ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+  home: HomePage(),
+  ));
+}
---
+++ search_gifs/lib/ui/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  _getSearch() async {
+    http.Response response;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
