30a916a0672fe2466a58cc0ab943daed4efeb52c
André Siqueira <siqueira.andre@gmail.com>
2019-08-19 07:33:57 -0300

    loading gifs

--- search_gifs/lib/main.dart
+++ search_gifs/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:search_gifs/ui/home_page.dart';
 
 void main() {
   runApp(MaterialApp(
-  home: HomePage(),
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
   ));
 }
